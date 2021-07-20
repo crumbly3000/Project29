42aa70f4b92f9546e311abbe6805a84648f3f26c
crumbly3000 <76978261+crumbly3000@users.noreply.github.com>
2021-07-20 09:27:28 -0400

    upload

---
+++ SlingShot.js
@@ -0,0 +1,21 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+       var options={
+           bodyA:bodyA,
+           pointB:pointB,
+           length:10,
+           stiffness:0.01
+       } 
+       this.chain = Matter.Constraint.create(options);
+       World.add(world, this.chain);
+    }
+    fly(){
+        this.chain.bodyA = null;
+    }
+
+    display(){
+        if(this.chain.bodyA){
+            line(this.chain.bodyA.position.x, this.chain.bodyA.position.y, this.pointB.x, this.pointB.y)
+        }
+    }
+}
